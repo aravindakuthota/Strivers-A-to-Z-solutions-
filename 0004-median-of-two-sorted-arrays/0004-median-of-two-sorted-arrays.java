ad76efe6afe59e4591af0c0cc233685efa8f13d6
Aravind Akuthota <aravindakuthota543@gmail.com>
2025-12-10 10:38:00 +0530

    Time: 1 ms (99.88%), Space: 48.8 MB (28.98%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,32 +1,54 @@
-// brute force solution 
-
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        ArrayList<Integer> arr = new ArrayList<>();
-       int i =0; int j =0;
+        
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int n = n1 + n2;
 
-       while(i < nums1.length && j < nums2.length) {
+        int ind2 = n / 2;      // second median index
+        int ind1 = ind2 - 1;   // first median index (only used when n is even)
 
-        if(nums1[i] < nums2[j]) 
-            arr.add(nums1[i++]);
-            else 
-              arr.add(nums2[j++]); 
- 
-       }
+        int i = 0, j = 0, cnt = 0;
+        int ind1_el = -1, ind2_el = -1;
 
-       while(i<nums1.length)
-         arr.add(nums1[i++]);
+        // Merge like sorted arrays
+        while (i < n1 && j < n2) {
+            int val;
 
-    while(j < nums2.length)
-    arr.add(nums2[j++]);
+            if (nums1[i] < nums2[j]) {
+                val = nums1[i++];
+            } else {
+                val = nums2[j++];
+            }
 
-    int n = arr.size();
+            if (cnt == ind1) ind1_el = val;
+            if (cnt == ind2) ind2_el = val;
 
+            cnt++;
+        }
 
-    if(n % 2 ==1 )
-        return arr.get(n/2);
-    return (arr.get(n / 2 - 1) + arr.get(n / 2)) / 2.0;
+        // Remaining nums1
+        while (i < n1) {
+            int val = nums1[i++];
 
+            if (cnt == ind1) ind1_el = val;
+            if (cnt == ind2) ind2_el = val;
 
+            cnt++;
+        }
+
+        // Remaining nums2
+        while (j < n2) {
+            int val = nums2[j++];
+
+            if (cnt == ind1) ind1_el = val;
+            if (cnt == ind2) ind2_el = val;
+
+            cnt++;
+        }
+
+        // Return median
+        if (n % 2 == 1) return ind2_el;  // odd length
+        return (ind1_el + ind2_el) / 2.0;
     }
 }
